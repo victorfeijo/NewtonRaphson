00f3ee2a147762985a37040e0d7001e92c15b14e
Victor <victor@localhost.localdomain>
2015-06-14 16:44:43 -0300

    Working

--- Interpol/coefSerieTaylor.m
+++ Interpol/coefSerieTaylor.m
@@ -1,6 +1,10 @@
 function c = coefSerieTaylor(n, a, b)
     c(n+1) = f(0, a, b);
-    for i=1 : n
-        c(n-i+1) = ((-1)^(i+1) * (b-a)^i * (b+a)^(-i))/i;
+    c(n) = 1/4* (((1/2)*(b+a))^(-1/2))*(b-a);
+    aux=1;
+
+    for i=2 : n
+        aux = aux * (-2*i + 3);
+        c(n-i+1) = (1/factorial(i)) * (1/2^(2*i)) * aux * (b-a)^i * ((1/2)*(b+a))^(-(2*i - 1)/2);
     end
 end
--- Interpol/series.m
+++ Interpol/series.m
@@ -13,17 +13,16 @@ sTaylor = fserieTaylor(tPlot, n, a, b);
 xPlot = 0.5 * (b-a).*tPlot + 0.5*(b+a);
 % Serie de Chebyschev
 %passo 1
-% 0.0123456790123457t³ -0.0555555555555556t² + 0.3333333333333333t¹ + 0.4054651081081644t⁰
+% 0.00283505757266572  -0.01701034543599429   0.20412414523193151   1.22474487139158894
 %passo 2 -> determinar chebyschev
-% 0.0123456790123457*(T3+3*T1)/4 - 0.0555555555555556*(T2+T0)/2 + 0.3333333333333333*T1 + 0.4054651081081644*T0
-% 0.377687330330387 T0 + 0.342592592592593 T1 - 0.0277777777777778 T2 + 0.00308641975308642 T3
+% 0.00283505757266572*(T3+3*T1)/4 - 0.01701034543599429*(T2+T0)/2 + 0.20412414523193151*T1 + 1.22474487139158894*T0
+% 1.21623969867359 T0+0.206250438411431 T1-0.0085051727179971 T2+0.00070876439316643 T3
 %passo 3 -> avaliar o erro de truncamento
 % erro de truncamento MCLAUREN(max) = 0.0156250000000000
 % erro de truncamento CHEBYSCHEV(max) = 0.00308641975308642 é menor que o erro de truncamento de mclauren
 %passo 4 -> pegar a série truncada e escrever em termos de x
-% 0.377687330330387 + 0.342592592592593 t¹ - 0.0277777777777778 (2t²-1)
-% -0.055555555555556t² + 0.342592592592593t¹ + 0.405465108108165
-coef = [-0.055555555555556  0.342592592592593 0.405465108108165];
+% 0.0028350575726657 t^3-0.017010345435994 t^2+0.20412414523193 t+1.2247448713916
+coef = [-0.017010345435994  0.2041241452319 1.2247448713916];
 % O grau do polinomio do chebyshev foi reduzido para 2
 sChebyschev = fserieCheby(tPlot, length(coef)-1, coef);
 % Calculo do valor exato
@@ -31,7 +30,7 @@ valorExato = sqrt(xPlot);
 % Calculo do gregory newton interpolado
 h = (b-a)/n;
 x = a : h : b
-y = log(x)
+y = sqrt(x)
 ygregorynewton = fgregorynewton(n, x, y, xPlot);
 erroInterpolador = abs(ygregorynewton - valorExato);
 erroSerieTaylor = abs(sTaylor - valorExato);
@@ -41,6 +40,7 @@ erroCheby = abs(sChebyschev - valorExato);
 
 sPade = fPade(tPlot, a, b);
 erroPade = abs(sPade - valorExato);
+erroMaxPade = max(erroPade)
 
 %plot( xPlot, erroInterpolador, "r;'Erro Interpolador';", xPlot, erroSerieTaylor, "b;'Erro Taylor';", xPlot, erroCheby, "k;'Erro Chebyschev';", xPlot, erroPade, "g;'Erro Padé';")
 plot( xPlot ,valorExato , "y;'Valor Exato';", xPlot , ygregorynewton, "r;'GregoryNewton';",xPlot , sTaylor, "b;'Serie Taylor';", xPlot , sChebyschev, "k;'ChebySchev';", xPlot , sPade, "g;'Padé';", 2.2,0.7)
